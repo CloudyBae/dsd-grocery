5161d304d96b2dacc68714192c203661f24dc603
Dina Rocio  Villanueva Quevedo <dinarocio@MacBook-Pro-de-Dina-2.local>
2024-04-02 00:20:10 -0500

    feat(): create basic styles for base button

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "./components/Button";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        id="add_to_basket_btn"
+        isFullWidth={true}
+        startEnhancer={<Text>✔️</Text>}
+        endEnhancer={<Text>$12.96</Text>}
+      >
+        Add to the Basket
+      </Button>
+      <Button onPress={() => console.log("click!")}>➕</Button>
+      <Button disabled={true}>Disable</Button>
+      <Button kind="ghost" onPress={() => console.log("click!")}>
+        Sign Up
+      </Button>
+      <View style={{ display: "flex", flexDirection:"row", alignItems:"center" }}>
+        <Button kind="outline" onPress={() => console.log("click!")}>
+          ➖
+        </Button>
+        <Text>    1    </Text>
+        <Button kind="outline" onPress={() => console.log("click!")}>
+          ➕
+        </Button>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
