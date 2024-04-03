02d72f2bdb76bbe2ac6795f83fdcd14331d22a71
clunstar <ccluney@gmail.com>
2024-04-03 17:02:14 -0500

    DSDGROC-21 Chris Cluney, I have created the AddProductCard component and added it to the component folder. I have also imported it to the App.js.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import AddProductCard from './components/AddProductCard';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AddProductCard />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/AddProductCard.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image, Pressable} from 'react-native';
+
+
+
+
+
+
+export default AddProductCard = (props) => {
+  
+const {onPress, title = '+' } = props;
+
+    return(
+        <View style={styles.container}>
+
+            <View>
+                <Image source={{
+                    uri: 'https://m.media-amazon.com/images/I/61fZ+YAYGaL._SL1500_.jpg'
+                }}
+                        style={styles.productImg}/>
+                
+                <Text style={styles.headingText}>Organic Bananas</Text>
+                <Text style={styles.amountText}>5pcs</Text>
+                
+                    <View style={styles.bottomCard}>
+                        
+                        <Pressable style={styles.addButton} onPress={props.onPress}>
+                            <Text style={styles.buttonText}>{title}</Text>
+                        </Pressable>
+                            
+                        
+
+                    </View>            
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        
+       padding: 10, 
+        backgroundColor: '#fff',
+        borderColor: '#e7e7e7',
+        borderWidth: 1,
+        borderRadius: 5,
+        height: 248,
+        width: 173,
+        top: 0,
+        left: 0,
+        
+
+    },
+    productImg: {
+    //    flex: 1,
+    paddingHorizontal: 60,
+        height: 110,
+        width: 80,
+    },
+    headingText: {
+        fontSize: 11,
+        fontWeight: 'bold',
+    },
+    amountText:{
+        fontSize: 10,
+        paddingTop: 4,
+    },
+    addButton: {
+        flex: 0,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#53b175',
+        borderColor: '#e7e7e7',
+        borderRadius: 5,
+        height: 50,
+        width: 50,
+        
+    },
+
+    buttonText:{
+        
+        
+
+        fontSize: 24,
+        color: '#ffffff'
+
+    },
+    bottomCard:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent:'flex-end',
+        paddingTop: 12,
+        
+        
+
+
+    },
+
+})
