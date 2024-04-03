ca61e1b843251c5909f12a89346fadf0c3f943fa
Dina Rocio  Villanueva Quevedo <dinarocio@MacBook-Pro-de-Dina-2.local>
2024-04-02 21:54:46 -0500

    feat(): build reusable cards

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainButton from './components/MainButton';
 import Macro from './components/Macro';
+import CategoryCard from './components/CategoryCard';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MainButton />
       <Macro macro={"Protein"} percentage={50} goal={100}/>
+      <CategoryCard  title="Dairy & Eggs" image='https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png'/>
+      <CategoryCard title="Frash Fruits & Vegetable" image='https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png'/>
+      <CategoryCard title="Dairy & Eggs" image='https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png'/>
     </View>
   );
 }
