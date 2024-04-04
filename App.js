1b759999791259c9cb569778b8ae7fd470c14b10
Christine Hoang <christinehoangt@gmail.com>
2024-04-03 19:52:43 -0500

    saving

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import MainButton from './components/MainButton';
 import Macro from './components/Macro';
 import Accordion from './components/Accordion';
+import Button from "./components/Button";
 
 export default function App() {
   return (
@@ -10,6 +11,17 @@ export default function App() {
       <MainButton />
       <Macro macro={"Protein"} percentage={50} goal={100}/>
       <Accordion/>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        id="add_to_basket_btn"
+        isFullWidth={true}
+        startEnhancer={<Text>✔️</Text>}
+        endEnhancer={<Text>$12.96</Text>}
+      >
+        Add to the Basket
+      </Button>
+      <MainButton />
+      <Macro macro={"Protein"} percentage={50} goal={100}/>
     </View>
   );
 }
@@ -17,8 +29,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
   },
 });
