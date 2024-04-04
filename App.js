c101e69b5d0a21d7f9c7b8dae927d6a9e3a47af4
Dina Rocio <dinarociovq15@hotmail.com>
2024-04-03 20:06:17 -0500

    chore(): create tests

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
-
 import { StyleSheet, Text, View } from "react-native";
 import Button from "./components/Button";
-import MainButton from './components/MainButton';
-import Macro from './components/Macro';
-import CategoryCard from './components/CategoryCard';
-
+import MainButton from "./components/MainButton";
+import Macro from "./components/Macro";
+import CategoryCard from "./components/CategoryCard";
 
 export default function App() {
   return (
@@ -19,10 +17,11 @@ export default function App() {
         Add to the Basket
       </Button>
       <MainButton />
-      <Macro macro={"Protein"} percentage={50} goal={100}/>
-      <CategoryCard  title="Dairy & Eggs" image='https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png'/>
-      <CategoryCard title="Frash Fruits & Vegetable" image='https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png'/>
-      <CategoryCard title="Dairy & Eggs" image='https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png'/>
+      <Macro macro={"Protein"} percentage={50} goal={100} />
+      <CategoryCard
+        title="Dairy & Eggs"
+        image="https://www.sunfreshfl.com/wp-content/uploads/2023/05/milk-and-eggs-580h-2.png"
+      />
     </View>
   );
 }
