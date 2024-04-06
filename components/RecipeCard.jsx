d28b570327ccfc022f739ae7977bc8f910addcaf
rudzzz <rudsolza13@gmail.com>
2024-04-06 15:44:18 -0300

    #DSDGROC-34 Fixed prettier auto formatting

--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -1,11 +1,11 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 
-const RecipeCard = ({title, image, minutes, rating}) => {
+const RecipeCard = ({ title, image, minutes, rating }) => {
   return (
     <View style={styles.cardContainer}>
       <View>
-        <Image style={styles.image} source={{uri: image || ''}}/>
+        <Image style={styles.image} source={{ uri: image || '' }} />
       </View>
       <View>
         <Text style={styles.cardTitle}>{title}</Text>
@@ -15,12 +15,12 @@ const RecipeCard = ({title, image, minutes, rating}) => {
         <Text>|</Text>
         <View style={styles.ratingDetails}>
           <Text>{rating || '0.0'}</Text>
-          <Entypo name="star" size={20} color="red" />
+          <Entypo name='star' size={20} color='red' />
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 export default RecipeCard;
 
@@ -31,27 +31,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 15,
-    gap: 3
+    gap: 3,
   },
   image: {
     height: 140,
     width: 140,
-    borderRadius: 15
+    borderRadius: 15,
   },
   cardDetails: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 3
+    gap: 3,
   },
   ratingDetails: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 1
+    gap: 1,
   },
   cardTitle: {
-    fontSize: 18
+    fontSize: 18,
   },
   cardSubTitle: {
-    fontSize: 14
+    fontSize: 14,
   },
 });
