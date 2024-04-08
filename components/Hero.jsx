2252c553973d0a1dc7969a31c20043e0a7cfde18
Christine Hoang <christinehoangt@gmail.com>
2024-04-08 11:49:06 -0500

    saving changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,14 +5,15 @@ const Hero = ({image}) => {
     <View>
         <Image source = {image}></Image>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
     image:{
         width: 200,
         height: 200
-    }
-})
+    },
+});
 
-export default Hero
+export default Hero;
+
