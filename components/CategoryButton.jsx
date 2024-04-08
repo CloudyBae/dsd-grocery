93a17148bd664f0a3e5890227e6046088455d7bc
rudzzz <rudsolza13@gmail.com>
2024-04-07 22:11:01 -0300

    auto formatting with prettier

--- components/CategoryButton.jsx
+++ components/CategoryButton.jsx
@@ -1,26 +1,33 @@
-import { StyleSheet, Text, TouchableOpacity } from "react-native"
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-const CategoryButton = ({title, onPress}) => {
+const CategoryButton = ({ title, onPress }) => {
   return (
-    <TouchableOpacity style={stlyes.categoryButton} 
-        onPress={onPress || (() => console.log("To use this button, ensure that you have set up a callback function (onPress)."))}
+    <TouchableOpacity
+      style={stlyes.categoryButton}
+      onPress={
+        onPress ||
+        (() =>
+          console.log(
+            'To use this button, ensure that you have set up a callback function (onPress).'
+          ))
+      }
     >
-      <Text style={stlyes.titleText}>{title || "Button"}</Text>
+      <Text style={stlyes.titleText}>{title || 'Button'}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default CategoryButton
+export default CategoryButton;
 
 const stlyes = StyleSheet.create({
   categoryButton: {
     padding: 15,
     margin: 5,
     backgroundColor: '#72C08F',
-    borderRadius: 13
+    borderRadius: 13,
   },
   titleText: {
     fontSize: 15,
-    color: '#0A0A0A'
+    color: '#0A0A0A',
   },
 });
