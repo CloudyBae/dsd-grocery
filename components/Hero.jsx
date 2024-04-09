5b453ea86882c9d37f0a8411b191907d28e378ab
Christine Hoang <christinehoangt@gmail.com>
2024-04-09 15:10:53 -0500

    image resizing has been corrected. The image will resize upon page refresh only

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,59 +1,24 @@
 import { Image, StyleSheet, View, Dimensions } from 'react-native';
-import Hero from 'react-native-hero';
-
+import HeroBanner from '../assets/hero.png';
 
 const myHero = ({ image }) => {
   const screenWidth = Dimensions.get('window').width;
   const aspectRatio = 4/3;
   const height = screenWidth/aspectRatio;
 
-// render() {
-//   return(
-//     <Hero
-//     source={image}
-//     renderOverlay={()=>(
-//       <View>
-//         <Text>Itsa Hero!</Text>
-//       </View>
-//     )} />
-//   )
-// }
-
-   return (
-    <View style = {styles.container}>
-       <Image source={image} style={{width: screenWidth, height}} resizeMode = "contain"/>
-     </View>
-   );
+  return (
+    <View style={styles.container}>
+      <Image source={HeroBanner} style={{width: screenWidth, height}} resizeMode='contain'/>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    overflow: 'hidden'
   },
 });
 
 export default myHero;
-
-// import React from 'react';
-// import { Text, View, StyleSheet, Dimensions } from 'react-native';
-// import Hero from 'react-native-hero';
-
-// const myHero = ({ image }) => {
-//   const screenWidth = Dimensions.get('window').width;
-
-//   return (
-//     <Hero
-//       source={image}
-//       renderOverlay={() => (
-//         <View>
-//           <Text>Itsa Hero!</Text>
-//         </View>
-//       )}
-//     />
-//   );
-// };
-
-// export default Hero;
