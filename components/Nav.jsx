72ee61e7bf0e2c572b2382e6d5dad282b31741f2
Christine Hoang <christinehoangt@gmail.com>
2024-04-09 14:29:28 -0500

    accessibility tags was added to nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,23 +5,36 @@ import { FontAwesome5 } from '@expo/vector-icons';
 export const Nav = () => {
   return (
     <View style={styles.nav}>
-      <TouchableOpacity onPress={() => console.log('Home button pressed')}>
+      <TouchableOpacity 
+        accessible={true} 
+        accessibilityLabel="Home button was pressed!"
+        onPress={() => console.log('Home button pressed')}>
         <FontAwesome5 name='home' style={styles.icon} />
       </TouchableOpacity>
       <TouchableOpacity
+        accessible={true} 
+        accessibilityLabel="Ingredients button was pressed!"
         onPress={() => console.log('Ingredient button pressed')}
       >
         <FontAwesome5 name='apple-alt' style={styles.icon} />
       </TouchableOpacity>
       <TouchableOpacity
+        accessible={true} 
+        accessibilityLabel="Shopping bag button was pressed!"
         onPress={() => console.log('Shopping bag button pressed')}
       >
         <FontAwesome5 name='shopping-bag' style={styles.icon} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => console.log('Account button pressed')}>
+      <TouchableOpacity 
+        accessible={true} 
+        accessibilityLabel="Account button was pressed!"
+        onPress={() => console.log('Account button pressed')}>
         <FontAwesome5 name='user-circle' style={styles.icon} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => console.log('Search button pressed')}>
+      <TouchableOpacity 
+        accessible={true} 
+        accessibilityLabel="Search button was pressed!"
+        onPress={() => console.log('Search button pressed')}>
         <FontAwesome5 name='search' style={styles.icon} />
       </TouchableOpacity>
     </View>
