34cd3f458d81249be7d3d0832aca156b5c99fd8f
Christine Hoang <christinehoangt@gmail.com>
2024-04-09 20:20:38 -0500

    accordion looks perfect in web browser. I'm having issues witht he CSS and titles showing up in the Accordion

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -1,106 +1,60 @@
-import React, { useState, useRef } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
-import { Animated } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
+import { ListItem } from '@rneui/themed';
+import { StyleSheet } from 'react-native-web';
 
 const Accordion = () => {
-  const [accordion, setAccordion] = useState([
+  const [expanded, setExpanded] = useState([]);
+
+  const list = [
     {
-      id: 1,
       title: 'Product Details',
-      details:
-        'Apples are nutritious. Apples may be good for weight loss. Apples may be good for your heart. As part of a heartful and varied diet.',
-      showContent: false,
+      details: 'Apples are nutritious. Apples may be good for weight loss. Apples may be good for your heart. As part of a heartful and varied diet.',
     },
     {
-      id: 2,
       title: 'Nutritions',
-      details:
-        'Calories: 94.6. Water: 156 grams. Protein: 0.43 grams. Carbs: 25.1 grams. Sugar: 18.9 grams. Fiber: 4.37 grams. Fat: 0.3 grams.',
-      showContent: false,
+      details: 'Calories 94.6. Water 156 grams. Protein 0.43 grams. Carbs 25.1 grams. Sugar 18.9 grams. Fiber 4.37 grams. Fat 0.3 grams.',
     },
     {
-      id: 3,
       title: 'Review',
       details: 'Truly, spectacularly delicious!',
-      showContent: false,
-    },
-  ]);
+    }
+  ];
 
-  const animationController = useRef(new Animated.Value(0)).current;
-
-  const toggleListItem = (index) => {
-    const newAccordion = [...accordion];
-    newAccordion[index].showContent = !newAccordion[index].showContent;
-    setAccordion(newAccordion);
-    animatedAccordion(index);
+  const toggleAccordion = (index) => {
+    const newExpanded = [...expanded];
+    newExpanded[index] = !newExpanded[index];
+    setExpanded(newExpanded);
   };
-
-  const animatedAccordion = (index) => {
-    const config = {
-      duration: 300,
-      toValue: accordion[index].showContent ? 1 : 0,
-      useNativeDriver: true,
-    };
-    Animated.timing(animationController, config).start();
-  };
-
-  const plusMinusToggle = (index) => {
-    return accordion[index].showContent ? '-' : '+';
-  };
-
-  const arrowTransform = animationController.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '90deg'],
-  });
 
   return (
-    <View style={styles.container}>
-      {accordion.map((item, index) => (
-        <TouchableOpacity key={item.id} onPress={() => toggleListItem(index)}>
-          <View style={styles.item}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.arrow}> {plusMinusToggle(index)} </Text>
-            {accordion[index].showContent && (
-              <Animated.View
-                style={{ transform: [{ rotateZ: arrowTransform }] }}
-              ></Animated.View>
-            )}
-            {accordion[index].showContent && (
-              <View style={styles.content}>
-                <Text>{item.details}</Text>
-              </View>
-            )}
-          </View>
-        </TouchableOpacity>
+    <ScrollView>
+      {list.map((item, index) => (
+        <ListItem.Accordion
+          key={index}
+          content={
+            <ListItem.Content>
+              <ListItem.Title>{item.title}</ListItem.Title>
+            </ListItem.Content>
+          }
+          isExpanded={expanded[index]}
+          onPress={() => toggleAccordion(index)}
+        >
+          <ListItem bottomDivider>
+            <ListItem.Content>
+              <ListItem.Subtitle style={styles.subtitle}>{item.details}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        </ListItem.Accordion>
       ))}
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    alignItems: 'left',
-    margin: 20,
-    width: 364,
-    height: 67,
-  },
-  item: {
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-    marginBottom: 5,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  arrow: {
-    position: 'absolute',
-    right: 10,
-    top: 0,
-  },
-  content: {
-    paddingLeft: 20,
+  subtitle: {
+    display: 'flex',
+    justifyContent: 'center',
   },
 });
 
