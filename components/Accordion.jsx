32924478b08fd0356115adb50a380de4fe27306f
Christine Hoang <christinehoangt@gmail.com>
2024-04-09 20:26:48 -0500

    prettier was ran

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -9,16 +9,18 @@ const Accordion = () => {
   const list = [
     {
       title: 'Product Details',
-      details: 'Apples are nutritious. Apples may be good for weight loss. Apples may be good for your heart. As part of a heartful and varied diet.',
+      details:
+        'Apples are nutritious. Apples may be good for weight loss. Apples may be good for your heart. As part of a heartful and varied diet.',
     },
     {
       title: 'Nutritions',
-      details: 'Calories 94.6. Water 156 grams. Protein 0.43 grams. Carbs 25.1 grams. Sugar 18.9 grams. Fiber 4.37 grams. Fat 0.3 grams.',
+      details:
+        'Calories 94.6. Water 156 grams. Protein 0.43 grams. Carbs 25.1 grams. Sugar 18.9 grams. Fiber 4.37 grams. Fat 0.3 grams.',
     },
     {
       title: 'Review',
       details: 'Truly, spectacularly delicious!',
-    }
+    },
   ];
 
   const toggleAccordion = (index) => {
@@ -42,7 +44,9 @@ const Accordion = () => {
         >
           <ListItem bottomDivider>
             <ListItem.Content>
-              <ListItem.Subtitle style={styles.subtitle}>{item.details}</ListItem.Subtitle>
+              <ListItem.Subtitle style={styles.subtitle}>
+                {item.details}
+              </ListItem.Subtitle>
             </ListItem.Content>
           </ListItem>
         </ListItem.Accordion>
