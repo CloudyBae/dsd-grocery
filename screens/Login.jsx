5776ddb6af5450f61ff1bc736038b3fc274bd455
Christine Hoang <christinehoangt@gmail.com>
2024-04-14 13:41:26 -0500

    added logo but I will change it to a grocery bag

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  View,
-} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View, Image, Dimensions } from 'react-native';
 import ThirdPartySignIn from '../components/ThirdPartySignIn';
+import LoginLogo from '../assets/Login.png';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export const LoginScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <View style={styles.loginContainer}>
+          <Image source={LoginLogo} style={{ width: screenWidth, height: screenHeight * 0.3 }} resizeMode='contain' />
           <ThirdPartySignIn
             title='Facebook'
             icon='facebook'
             textColor='white'
             iconColor='white'
             backgroundColor='blue'
+            style= {styles.loginButton}
             onPress={() => console.log('Facebook')}
           />
           <ThirdPartySignIn
@@ -26,6 +27,7 @@ export const LoginScreen = () => {
             icon='google'
             iconColor='black'
             backgroundColor='white'
+            style = {styles.loginButton}
             onPress={() => console.log('Google')}
           />
           <ThirdPartySignIn
@@ -34,6 +36,7 @@ export const LoginScreen = () => {
             textColor='white'
             iconColor='white'
             backgroundColor='black'
+            style = {styles.loginButton}
             onPress={() => console.log('Apple')}
           />
         </View>
@@ -44,7 +47,7 @@ export const LoginScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#52B175',
+    backgroundColor: '#72C08F',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -57,4 +60,7 @@ const styles = StyleSheet.create({
     marginTop: 25,
     marginBottom: 50,
   },
+  loginButton:{
+    padding: 10
+  }
 });
