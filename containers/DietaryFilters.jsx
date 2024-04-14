182dca889945131cefed8c4ec303f761ece2f69a
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-04-14 15:39:24 -0500

    Committing Dietary filters that are mapped from list of categories

--- containers/DietaryFilters.jsx
+++ containers/DietaryFilters.jsx
@@ -1,254 +1,98 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { CheckBox, ListItem } from '@rneui/themed';
-import FlexContainer from '../containers/FlexContainer';
-/*
-const list = [
-  {
-    id:0,
-    label:'Vegetarian',
-    checked: false
-  },
-  {
-    id:1,
-    label:'Vegan',
-    checked: false
-  },
-  {
-    id:2,
-    label:'Paleo',
-    checked: false
-  },
-  {
-    id:3,
-    label:'High-Fiber',
-    checked: false
-  },
-  {
-    id:4,
-    label:'High-Protein',
-    checked: false
-  },
-  {
-    id:5,
-    label:'Low-Carb',
-    checked: false
-  },
-  {
-    id:6,
-    label:'Low-Fat',
-    checked: false
-  },
-  {
-    id:7,
-    label:'Low-Sodium',
-    checked: false
-  },
-  {
-    id:8,
-    label:'Low-Sugar',
-    checked: false
-  },
-  {
-    id:9,
-    label:'Alcohol-Free',
-    checked: false
-  },
-  {
-    id:10,
-    label:'Balanced',
-    checked: false
-  },
-  {
-    id:11,
-    label:'Immunity',
-    checked: false
+import { SPOONACULAR_API_KEY } from '@env';
+
+
+export default function DietaryFilters() {
+
+  const [categories, setCategories] = useState([
+    {
+      id:0,
+      label:'Vegetarian',
+      checked: false
+    },
+    {
+      id:1,
+      label:'Vegan',
+      checked: false
+    },
+    {
+      id:2,
+      label:'Paleo',
+      checked: false
+    },
+    {
+      id:3,
+      label:'High-Fiber',
+      checked: false
+    },
+    {
+      id:4,
+      label:'High-Protein',
+      checked: false
+    },
+    {
+      id:5,
+      label:'Low-Carb',
+      checked: false
+    },
+    {
+      id:6,
+      label:'Low-Fat',
+      checked: false
+    },
+    {
+      id:7,
+      label:'Low-Sodium',
+      checked: false
+    },
+    {
+      id:8,
+      label:'Low-Sugar',
+      checked: false
+    },
+    {
+      id:9,
+      label:'Alcohol-Free',
+      checked: false
+    },
+    {
+      id:10,
+      label:'Balanced',
+      checked: false
+    },
+    {
+      id:11,
+      label:'Immunity',
+      checked: false
+    }
+  ]);
+
+  const onValueChange = (item, index) => {
+    const newCategory = [...categories];
+    newCategory[index].isCheck = !item.isCheck;
+    setCategories(newCategory);
   }
-]
 
-this.state = {
-  list: list
-}
-*/
-const DietaryFilters = () => {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.title}>Dietary Requirements</Text>
 
-  const [checked, setChecked] = React.useState([false, false]);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Dietary Preferences</Text>
-      <FlexContainer>
-      <ListItem>
-        <ListItem.CheckBox
-          // Use ThemeProvider to change the defaults of the checkbox
-          checkedColor='#52B175'
-          iconType="material-community"
-          checkedIcon="checkbox-marked"
-          uncheckedIcon="checkbox-blank-outline"
-          checked={checked[0]}
-          onPress={() => setChecked([!checked[0], checked[1]])}
-        />
-        <ListItem.Content>
-          <ListItem.Title>Vegetarian</ListItem.Title>
-        </ListItem.Content>
-      </ListItem>
-      <ListItem>
-        <ListItem.CheckBox
-          // Use ThemeProvider to change the defaults of the checkbox
-          checkedColor='#52B175'
-          iconType="material-community"
-          checkedIcon="checkbox-marked"
-          uncheckedIcon="checkbox-blank-outline"
-          checked={checked[1]}
-          onPress={() => setChecked([checked[0], !checked[1]])}
-        />
-        <ListItem.Content>
-          <ListItem.Title>Vegan</ListItem.Title>
-        </ListItem.Content>
-      </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Paleo</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>High-Fiber</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>High-Protein</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Low-Carb</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Low-Fat</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Low-Sodium</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Low-Sugar</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Alcohol-Free</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Balanced</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem>
-          <ListItem.CheckBox
-            // Use ThemeProvider to change the defaults of the checkbox
-            checkedColor='#52B175'
-            iconType="material-community"
-            checkedIcon="checkbox-marked"
-            uncheckedIcon="checkbox-blank-outline"
-            checked={checked[1]}
-            onPress={() => setChecked([checked[0], !checked[1]])}
-          />
-          <ListItem.Content>
-            <ListItem.Title>Immunity</ListItem.Title>
-          </ListItem.Content>
-        </ListItem>
-      </FlexContainer>
-    </View>
-  );
-};
+          {categories.map((item, index) => {
+            return <CheckBox
+              // Use ThemeProvider to change the defaults of the checkbox
+              checkedColor='#52B175'
+              title={item.label}
+              checked={item.isCheck || false}
+              onPress={(val) => onValueChange(item, index)}
+              key={item.name}
+            />
+          })}
+        </View>
+      )
+  }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -263,6 +107,4 @@ const styles = StyleSheet.create({
     fontFamily: 'Gilroy-Bold',
     fontSize: 24,
   },
-});
-
-export default DietaryFilters;
+})
