b6a5832b5213b4339b6b9922ead92b1860d89a98
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-04-16 16:39:27 -0500

    Finished styling on Account settings header

--- components/AcctHeader.jsx
+++ components/AcctHeader.jsx
@@ -1,21 +1,34 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const AcctHeader = () => {
   const name = 'John Smith';
-  const email = 'johnsmith@gmail.com';
+  const email = 'john-smith@gmail.com';
   return (
     <View style={styles.headerContainer}>
       <View style={styles.userImg}>
-        <Image 
+        <Image
           source={require('../assets/Avatar.png')}
-          style={{width: 120, height: 120, borderRadius: 150 / 2,}}
+          style={{ width: 60, height: 60, borderRadius: 150 / 2 }}
         />
       </View>
 
-      <View style={styles.header}>
-        <Text style={styles.name}>{name}</Text>
-        <Text style={styles.email}>{email}</Text>
+      <View style={styles.header} numberOfLines={null}>
+        <Text style={styles.name} numberOfLines={null}>
+          {name}{' '}
+          <TouchableOpacity
+            style={styles.iconContainer}
+            onPress={() => console.log('Edit Icon Clicked')}
+          >
+            <MaterialCommunityIcons
+              name='lead-pencil'
+              size={20}
+              color='#52B175'
+            />
+          </TouchableOpacity>
+        </Text>
+        <Text style={styles.email} numberOfLines={null}>{email}</Text>
       </View>
     </View>
   );
@@ -24,25 +37,39 @@ const AcctHeader = () => {
 const styles = StyleSheet.create({
   headerContainer: {
     flexDirection: 'row',
+    paddingTop: 40,
+    paddingBottom: 40,
+    borderTopColor: 'white',
+    borderRightColor: 'white',
+    borderBottomColor: '#52B175',
+    borderLeftColor: 'white',
+    borderWidth: 3,
+    backgroundColor: 'white'
   },
   header: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-end',
-    paddingBottom: 50,
   },
   name: {
-    fontSize: 56,
+    flex: 1,
+    fontSize: 24,
     fontWeight: 'bold',
   },
   email: {
-    fontSize: 36,
+    flex: 1,
+    fontSize: 12,
     fontWeight: 'bold',
+  },
+  iconContainer: {
+    height: 20,
+    width: 20,
   },
   userImg: {
     marginLeft: 30,
-    marginRight: 60,
-    overflow: "hidden",
-  }
+    marginRight: 10,
+    overflow: 'hidden',
+  },
 });
 
 export default AcctHeader;
