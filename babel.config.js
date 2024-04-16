d019bfb78411b8d490b496d9f889f01d101a4089
Christine Hoang <christinehoangt@gmail.com>
2024-04-16 10:47:21 -0500

    babel config and urls.py adjusted to match main branch

--- babel.config.js
+++ babel.config.js
@@ -4,14 +4,12 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       ["module:react-native-dotenv", {
-        "envName": 'APP_ENV',
         "moduleName": "@env",
         "path": ".env",
         "blacklist": null,
         "whitelist": null,
         "safe": false,
-        "allowUndefined": true,
-        "verbose": false,
+        "allowUndefined": true
       }]
     ]
   };
--- zbackend/api/urls.py
+++ zbackend/api/urls.py
@@ -1,11 +1,10 @@
 # Example code of api/urls.py
 # Remove this example and write your own code
-
-''''
+"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/<int:userId>/ingredients', views.add_ingredient, name='add_ingredient'),
+    path('ingredients/', views.IngredientListCreateAPIView.as_view()),
 ]
-'''
+"""
