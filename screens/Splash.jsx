b3f63f69ceddf308e609497a9e4aae8e2a216358
Christine Hoang <christinehoangt@gmail.com>
2024-04-16 14:57:07 -0500

    I made a basic splash screen, a button from the Login page that will route to Home

--- screens/Splash.jsx
+++ screens/Splash.jsx
@@ -1,9 +1,67 @@
-import { Text } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import { Title } from '../components/Typography/index.js';
+import { useNavigation } from '@react-navigation/native';
 
 export const SplashScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <>
-      <Text>splash!!</Text>
-    </>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.splashContainer}>
+        <Title style={styles.title}>Zest</Title>
+        <Title style={styles.subtitle}>online groceries</Title>
+        <TouchableOpacity
+          accessible={true}
+          accessibilityLabel='Next'
+          onPress={() => navigation.navigate('Login')}
+          style={styles.button}
+        >
+          <Title style={styles.buttonText}>Next</Title>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#72C08F',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splashContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 40,
+    color: '#FFF',
+    lineHeight: 60,
+  },
+  subtitle: {
+    marginBottom: 20,
+    fontSize: 24,
+    color: '#FFF',
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#72C08F',
+  },
+});
+
+export default SplashScreen;
