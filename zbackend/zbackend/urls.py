60cb7bb4d0d32894ccf1c45b748024d21406b1e5
rudzzz <rudsolza13@gmail.com>
2024-04-17 16:32:48 -0300

    #DSDGROC-70 - Removing duplicate line

--- zbackend/zbackend/urls.py
+++ zbackend/zbackend/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("accounts/", include("allauth.urls")),
-    path("api/", include("api.urls")),
 ]
