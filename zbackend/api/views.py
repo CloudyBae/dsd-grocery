1f61f07344c746259fff4d144fe4d6c686b62eb9
Ren Das <aaryan.das.42gmail.com>
2024-04-17 15:13:43 -0500

    Fixed format/lint

--- zbackend/api/views.py
+++ zbackend/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, BasePermission
 
 from .models import ShoppingList
