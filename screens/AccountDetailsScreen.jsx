23454f573c0c8051956234e2165c8cba57996d4e
clunstar <ccluney@gmail.com>
2024-04-17 09:43:45 -0500

    First commit for the AccountDetailsScreen

--- screens/AccountDetailsScreen.jsx
+++ screens/AccountDetailsScreen.jsx
@@ -19,100 +19,96 @@ const AccountDetailsScreen = () => {
   const handleDelete = () => {
     // Perform delete action, like resetting the username to default
     setUsername('Account Username');
-    setEmail('example@example.com')
+    setEmail('example@example.com');
     setIsEditing(false);
   };
 
   return (
     <View style={styles.wholeContainer}>
-        <AcctHeader />
-    <View style={styles.container}>
-        
+      <AcctHeader />
+      <View style={styles.container}>
         <View styles={styles.inputSection}>
-        <View style={styles.textBoxContainer}>
-        <Text style={styles.myDetails}>My Details</Text>
-        <View
-        style={{
-          width: '100%',
-          height: 1,
-          backgroundColor: '#96d1ab',
-          alignSelf: 'center',
-          marginTop: 10,
-          marginBottom: 15,
-        }}
-      />
-      <Text style={styles.myDetails}>Username</Text>
-      <TextInput
-        style={styles.textBox}
-        value={username}
-        onChangeText={text => setUsername(text)}
-        placeholder="Account Username"
-        editable={isEditing}
-      />
-     
-    </View>
-    <View style={styles.textBoxContainer}>
-        <Text style={styles.myDetails}>Email</Text>
-        <TextInput
-          style={styles.textBox}
-          value={email}
-          onChangeText={text => setEmail(text)}
-          placeholder="Email"
-          editable={isEditing}
-        />
-      </View>
-      </View>
+          <View style={styles.textBoxContainer}>
+            <Text style={styles.myDetails}>My Details</Text>
+            <View
+              style={{
+                width: '100%',
+                height: 1,
+                backgroundColor: '#96d1ab',
+                alignSelf: 'center',
+                marginTop: 10,
+                marginBottom: 15,
+              }}
+            />
+            <Text style={styles.myDetails}>Username</Text>
+            <TextInput
+              style={styles.textBox}
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+              placeholder='Account Username'
+              editable={isEditing}
+            />
+          </View>
+          <View style={styles.textBoxContainer}>
+            <Text style={styles.myDetails}>Email</Text>
+            <TextInput
+              style={styles.textBox}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              placeholder='Email'
+              editable={isEditing}
+            />
+          </View>
+        </View>
 
-      <View style={styles.editSection}>
-      {!isEditing ? (
-        <Button title="Edit" onPress={handleEdit} />
-      ) : (
-        <>
-          <Button title="Save" onPress={handleSave} />
-          <Button title="Delete" onPress={handleDelete} />
-        </>
-      )}
-
+        <View style={styles.editSection}>
+          {!isEditing ? (
+            <Button title='Edit' onPress={handleEdit} />
+          ) : (
+            <>
+              <Button title='Save' onPress={handleSave} />
+              <Button title='Delete' onPress={handleDelete} />
+            </>
+          )}
+        </View>
       </View>
-    </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-wholeContainer: {
-backgroundColor: '#fff',
-height:'100%',
-},    
-container: {
-flexDirection: 'row',
-justifyContent: 'center',
-alignItems: 'center',
-backgroundColor: '#fff',
-marginTop: 30,
-},    
-inputSection: {
-flexDirection: 'column',
-width: '100%',
-},
-myDetails:{
-fontWeight: 'bold',
-width: 200,
-},
-textBoxContainer: {
-alignItems: 'center', 
-},    
-textBox: {
-    flex: 0, 
+  wholeContainer: {
+    backgroundColor: '#fff',
+    height: '100%',
+  },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginTop: 30,
+  },
+  inputSection: {
+    flexDirection: 'column',
+    width: '100%',
+  },
+  myDetails: {
+    fontWeight: 'bold',
+    width: 200,
+  },
+  textBoxContainer: {
+    alignItems: 'center',
+  },
+  textBox: {
+    flex: 0,
     borderColor: '#96d1ab',
-    borderBottomWidth: 1, 
+    borderBottomWidth: 1,
     marginBottom: 5,
-    padding: 10, 
+    padding: 10,
     width: 200,
     height: 80,
-},
-editSection: {
-marginLeft: 20,
-},
-
+  },
+  editSection: {
+    marginLeft: 20,
+  },
 });
 export default AccountDetailsScreen;
