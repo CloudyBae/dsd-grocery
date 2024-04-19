b22b496f96cefc5eeb58a5278e9984c4fdd7fed4
Christine Hoang <christinehoangt@gmail.com>
2024-04-19 14:02:32 -0500

    made changes to Ingredients Model and Serializers to include image field. Also worked on mapping mock data in ShoppingList

--- components/ShoppingListItem.jsx
+++ components/ShoppingListItem.jsx
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 const ShoppingListItem = ({ items }) => {
+  const [shoppingListItem, setShoppingListItem] = useState([]);
+
+  useEffect(() => {
+    const data = [
+      {
+        id: 1,
+        user: 2,
+        ingredient: {
+          id: 6,
+          name: 'Milk',
+          image: 'https://m.media-amazon.com/images/I/41uC0xBoZ3L._SX300_SY300_QL70_FMwebp_.jpg',
+          quantity: '1.00',
+          user: 2,
+          preference: 1,
+        },
+        quantity: "5.00",
+        is_purchased: false,
+      },
+      {
+        id: 2,
+        user: 2,
+        ingredient: {
+          id: 2,
+          name: "GreenBellPepper",
+          image: 'https://m.media-amazon.com/images/I/41ultdsxF8L._SY300_SX300_QL70_FMwebp_.jpg',
+          quantity: "1.00",
+          user: 2,
+          preference: 1
+        },
+        quantity: "2.00",
+        is_purchased: false,
+      },
+      {
+        id: 3,
+        user: 2,
+        ingredient: {
+          id: 1,
+          name: 'Apples',
+          image: 'https://m.media-amazon.com/images/I/413SS6wy+cL._SY300_SX300_.jpg',
+          quantity: '1.00',
+          user: 2,
+          preference: 1,
+        },
+        quantity: "3.00",
+        is_purchased: false, 
+      },
+    ];
+
+    const filteredItems = data.filter(item => !item.is_purchased);
+
+    filteredItems.forEach(item => {
+      item.quantity = parseInt(item.quantity);
+    });
+    setShoppingListItem(filteredItems);
+  }, []);
+
   return (
     <View>
-      {items.map((item, index) => (
+      {shoppingListItem.map((item, index) => (
         <View key={index} style={styles.container}>
           <View style={styles.firstSection}>
             <Image
-              source={{ uri: item.image || '' }}
+              source={{ uri: item.ingredient.image || '' }}
               style={styles.productImg}
               resizeMode='cover'
             />
           </View>
           <View style={styles.textSection}>
-            <Text style={styles.headingText}>{item.name}</Text>
+            <Text style={styles.headingText}>{item.ingredient.name}</Text>
             <View style={styles.amountContainer}>
-              <Text style={styles.amountText}>{item.qty}</Text>
-              <Text style={styles.unitText}>{item.unit}</Text>
+              <Text style={styles.amountText}>{item.quantity}</Text>
+              <Text style={styles.unitText}>quantity</Text>
             </View>
           </View>
         </View>
@@ -35,18 +91,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     backgroundColor: '#fff',
     height: 180,
-    marginBottom: 10, // Add margin between items
+    marginBottom: 10,
   },
   firstSection: {
     flexDirection: 'column',
     alignItems: 'center',
     paddingLeft: 5,
-  },
-  tagImg: {
-    height: 40,
-    width: 120,
-    borderRadius: 10,
-    marginBottom: 2,
   },
   productImg: {
     height: 110,
