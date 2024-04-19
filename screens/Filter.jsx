bcfdb77d46025a3e28bd6c205ac313545fedac15
Christine Hoang <christinehoangt@gmail.com>
2024-04-18 22:16:33 -0500

    prettier ran

--- screens/Filter.jsx
+++ screens/Filter.jsx
@@ -1,12 +1,11 @@
 import { Text } from 'react-native';
 import Nav from '../components/Nav';
 
-
 export const FilterScreen = () => {
   return (
     <>
       <Text>filter</Text>
-      <Nav/>
+      <Nav />
     </>
   );
 };
