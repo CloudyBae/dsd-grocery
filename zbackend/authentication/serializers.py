dad9395606281e956b9e65d68d826b2a0a0d2bb2
Ren Das <aaryan.das.42gmail.com>
2024-04-19 05:29:54 -0500

    authentication update

--- zbackend/authentication/serializers.py
+++ zbackend/authentication/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-class GoogleAuthSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    name = serializers.CharField()
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['id', 'email', 'name', 'dietary_preferences']
+        extra_kwargs = {
+            'dietary_preferences':{'required':False}
+        }
--- zbackend/authentication/urls.py
+++ zbackend/authentication/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import GoogleAuthView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'profile', UserViewSet, basename='user-profile')
+
 
 urlpatterns = [
-    path("authentication/google-auth/", GoogleAuthView.as_view(), name="google-auth"),
+    path('', include(router.urls)),
 ]
