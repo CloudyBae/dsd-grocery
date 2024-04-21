6fc9bf1536738999787fba559d8a3d36ab3540ca
Stephanie Thompson <43892794+Rocklobster84@users.noreply.github.com>
2024-04-21 17:11:22 -0500

    It's not pretty, but the filter array is writing to my test file now

--- screens/DietaryAllergenFilter.jsx
+++ screens/DietaryAllergenFilter.jsx
@@ -1,42 +1,110 @@
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
-import DietaryFilters from '../containers/DietaryFilters';
-import AllergenFilters from '../containers/AllergenFilters';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, View, Text } from 'react-native';
 import AcctHeader from '../components/AcctHeader';
-import MainButton from '../components/MainButton';
+import { CheckBox } from '@rneui/themed';
+import CategoryButton from '../components/CategoryButton';
 import Nav from '../components/Nav';
 import { useNavigation } from '@react-navigation/native';
 
 export const DietaryAllergenFilterScreen = () => {
   const navigation = useNavigation();
 
-  const getDietaryFiltersData = () => {
-      
+  const [categories, setCategories] = useState([
+    {
+      id: 0,
+      preference_name: 'Gluten Free',
+      key: 'Gluten Free',
+      isCheck: false,
+    },
+    {
+      id: 1,
+      preference_name: 'Ketogenic',
+      key: 'Ketogenic',
+      isCheck: false,
+    },
+    {
+      id: 2,
+      preference_name: 'Vegetarian',
+      key: 'Vegetarian',
+      isCheck: false,
+    },
+    {
+      id: 3,
+      preference_name: 'Lacto-Vegetarian',
+      key: 'Lacto-Vegetarian',
+      isCheck: false,
+    },
+    {
+      id: 4,
+      preference_name: 'Ovo-Vegetarian',
+      key: 'Ovo-Vegetarian',
+      isCheck: false,
+    },
+    {
+      id: 5,
+      preference_name: 'Vegan',
+      key: 'Vegan',
+      isCheck: false,
+    },
+    {
+      id: 6,
+      preference_name: 'Pescetarian',
+      key: 'Pescetarian',
+      isCheck: false,
+    },
+    {
+      id: 7,
+      preference_name: 'Paleo',
+      key: 'Paleo',
+      isCheck: false,
+    },
+    {
+      id: 8,
+      preference_name: 'Primal',
+      key: 'Primal',
+      isCheck: false,
+    },
+    {
+      id: 9,
+      preference_name: 'Low FODMAP',
+      key: 'Low FODMAP',
+      isCheck: false,
+    },
+    {
+      id: 10,
+      preference_name: 'Whole30',
+      key: 'Whole30',
+      isCheck: false,
+    },
+  ]);
+
+  const onValueChange = (item, index) => {
+    const newCategory = [...categories];
+    newCategory[index].isCheck = !item.isCheck;
+    setCategories(newCategory);
+    return newCategory;
   };
 
-  const handleSave = async () => {
-    try {
-      const dietaryFiltersData = await getDietaryFiltersData();
+  const handleSave = () => {
+    const dietaryFiltersData = categories;
 
-      const filterData = {
-        dietaryFilters: dietaryFiltersData,
-      };
+    const filterData = {
+      dietaryFilters: dietaryFiltersData,
+    };
 
-      const response = await fetch('http://localhost:3001/filters', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(filterData),
-      });
+    const response = fetch('http://localhost:3001/filters', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(filterData),
+    });
 
-      if (!response.ok) {
-        throw new Error('Failed to save filters');
-      }
+    //if (!response.ok) {
+    //  throw new Error('Failed to save filters');
+    //}
 
-      console.log('Filters saved successfully');
-    } catch (error) {
-      console.error('Error saving filters:', error.message);
-    }
+    //console.log('Filters saved successfully');
   };
 
   return (
@@ -45,11 +113,28 @@ export const DietaryAllergenFilterScreen = () => {
         <ScrollView>
           <View style={styles.homeContainer}>
             <AcctHeader />
-            <DietaryFilters/>
+            <View style={styles.container}>
+              <Text style={styles.title}>Dietary Requirements</Text>
+
+              {categories.map((item, index) => {
+                return (
+                  <CheckBox
+                    // Use ThemeProvider to change the defaults of the checkbox
+                    checkedColor='#52B175'
+                    backgroundColor='#f0f0f0'
+                    title={item.preference_name}
+                    checked={item.isCheck || false}
+                    onPress={() => onValueChange(item, index)}
+                    key={item.key}
+                    name={item.name}
+                    id={item.id}
+                  />
+                );
+              })}
+            </View>
 
             <View style={styles.buttonContainer}>
-              <MainButton title='Save' 
-                onPress={(handleSave)}/>
+              <CategoryButton title='Save' onPress={() => handleSave()} />
             </View>
           </View>
         </ScrollView>
@@ -69,6 +154,18 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     alignItems: 'center',
     backgroundColor: 'rgb(255,255,255)',
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'rgb(255,255,255)',
+  },
+  title: {
+    height: 60,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
 
