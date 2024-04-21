8a948cf3ba998d7a6d3aab1f8ccbf215ed22af79
Yoojin93 <yoojinbae93@gmail.com>
2024-04-21 15:17:05 -0500

    format

--- zbackend/api/views.py
+++ zbackend/api/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, BasePermission
+# from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import BasePermission
 from django.http import JsonResponse
 from .external_apis import (
     get_recipes_by_dietary_preferences,
