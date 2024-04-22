ad3887b6d9ea36639a97a9f2db9891ad02b3369f
Yoojin93 <yoojinbae93@gmail.com>
2024-04-22 18:48:20 -0500

    missed docker file

--- zbackend/zbackend/settings.py
+++ zbackend/zbackend/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
         "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "PORT": 5432,
     }
 }
 
