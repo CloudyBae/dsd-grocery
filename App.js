9a5fd4c06754f0b8fc9a114e8ccbf227162c41ef
Ryan Furrer <ryandotfurrer@gmail.com>
2024-04-22 15:55:34 -0400

    Organize and re-import all screens in

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { AccountDetailsScreen } from './screens/AccountDetailsScreen';
+import { AccountSettingsMenu } from './screens/AccountSettingsMenu';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Filter } from './screens/Filter';
 import { HomeScreen } from './screens/Home';
 import { IngredientScreen } from './screens/Ingredient';
-import { RecipeScreen } from './screens/Recipe';
-import { Filter } from './screens/Filter';
-import { ShoppingListScreen } from './screens/ShoppingList';
 import { LoginScreen } from './screens/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { RecipeScreen } from './screens/Recipe';
+import { ShoppingListScreen } from './screens/ShoppingList';
 import { SignUpScreen } from './screens/SignUp';
-import { AccountSettingsMenu } from './screens/AccountSettingsMenu';
-import { AccountDetailsScreen } from './screens/AccountDetailsScreen';
-import { AddIngredient } from './screens/AddIngredient';
+import {AcctSavedRecipesScreen} from './screens/AcctSavedRecipesScreen'
+import {DietaryAllergenFilter} from './screens/DietaryAllergenFilter'
+
 
 const Stack = createNativeStackNavigator();
 
@@ -19,14 +21,23 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen
-          name='Home'
-          component={HomeScreen}
+          name='Account Details'
+          component={AccountDetailsScreen}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen
+          name='AccountSettingsMenu'
+          component={AccountSettingsMenu}
           options={{ headerShown: false }}
         />
-
         <Stack.Screen
-          name='Recipe'
-          component={RecipeScreen}
+          name='Filter'
+          component={Filter}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
@@ -35,39 +46,14 @@ function App() {
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Filter'
-          component={FilterScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='ShoppingList'
-          component={ShoppingListScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='AccountSettingsMenu'
-          component={AccountSettingsMenu}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
           name='Login'
           component={LoginScreen}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Signup'
-          component={SignUpScreen}
+          name='Recipe'
+          component={RecipeScreen}
           options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='Home'
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='Ingredients'
-          component={IngredientScreen}
-          options={{ headerShown: true }}
         />
         <Stack.Screen
           name='Shopping List'
@@ -75,35 +61,19 @@ function App() {
           options={{ headerShown: true }}
         />
         <Stack.Screen
-          name='Account'
-          component={AccountSettingsMenu}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name='Dietary Preferences & Allergies'
-          component={DietaryAllergenFilterScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name='Account Details'
-          component={AccountDetailsScreen}
-          options={{ headerShown: true }}
+          name='Signup'
+          component={SignUpScreen}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name='Saved Recipes'
           component={AcctSavedRecipesScreen}
           options={{ headerShown: true }}
         />
-
         <Stack.Screen
-          name='Recipe'
-          component={RecipeScreen}
+          name='Dietary Preferences & Allergies'
+          component={DietaryAllergenFilter}
           options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name='Filter'
-          component={Filter}
-          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
